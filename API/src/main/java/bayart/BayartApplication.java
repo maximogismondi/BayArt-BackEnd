1562ee31fa994f49df50facb3c1debe4ae9c710c
MaximoGismondi <63014012+MaximoGismondi@users.noreply.github.com>
2020-10-06 13:17:28 -0300

    Creo la API

---
+++ API/src/main/java/bayart/BayartApplication.java
@@ -0,0 +1,13 @@
+package bayart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayartApplication.class, args);
+    }
+
+}
---
+++ API/src/test/java/bayart/BayartApplicationTests.java
@@ -0,0 +1,13 @@
+package bayart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
